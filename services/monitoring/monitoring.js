aac353d87c695fbe774177976bc11f014fb0f953
Andrii Fedorenko <a1349252@unet.univie.ac.at>
2017-09-04 22:32:30 +0200

    test

--- services/monitoring/monitoring.js
+++ services/monitoring/monitoring.js
@@ -3,16 +3,16 @@
 let moment = require('moment');
 
 function currentTimeStamp() {
-    let now = moment();
-    // let now = moment().subtract(2, 'hours');
+    // let now = moment().subtract(3, 'hours');
+    let now = moment().subtract(2, 'hours');
     let years = now.format('YYYY-MM-DD');
     let timestamp = now.format('HH:mm:ss');
     return years + 'T' + timestamp + 'Z';
 }
 
 function currentStampMinusTime(minus, type) {
-    let now = moment().subtract(minus, type);
-    // let now = moment().subtract(2, 'hours').subtract(minus, type);
+    // let now = moment().subtract(3, 'hours').subtract(minus, type);
+    let now = moment().subtract(2, 'hours').subtract(minus, type);
     let years = now.format('YYYY-MM-DD');
     let timestamp = now.format('HH:mm:ss');
     return years + 'T' + timestamp + 'Z';
