6fd3e3f77b144b0a2590a8e43c5f232a5691c7f3
Andrii Fedorenko <a1349252@unet.univie.ac.at>
2017-09-28 12:11:21 +0200

    prod

--- services/server.js
+++ services/server.js
@@ -13,7 +13,7 @@ app.use(bodyParser.json());
 app.use(function (req, res, next) {
     res.setHeader('Content-Type', 'application/json');
 
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8888');
+    res.setHeader('Access-Control-Allow-Origin', 'http://131.130.37.20:9594');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
     res.setHeader('Access-Control-Allow-Credentials', true);
