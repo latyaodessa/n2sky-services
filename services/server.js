0f4850e0092b3744cc20bdf97965c25ac3e490b8
Andrii Fedorenko <a1349252@unet.univie.ac.at>
2017-09-28 15:05:20 +0200

    origin

--- services/server.js
+++ services/server.js
@@ -13,16 +13,14 @@ app.use(bodyParser.json());
 app.use(function (req, res, next) {
     res.setHeader('Content-Type', 'application/json');
 
-    let allowedOrigins = ['http://131.130.37.20:9594', 'http://localhost:8888/'];
-    let origin = req.headers.origin;
+    // let allowedOrigins = ['http://131.130.37.20:9594', 'http://localhost:8888/'];
+    // let origin = req.headers.origin;
+    //
+    // if(allowedOrigins.indexOf(origin) > -1){
+    //     res.setHeader('Access-Control-Allow-Origin', origin);
+    // }
 
-    // console.log(origin, "asdasdasd");
-
-    if(allowedOrigins.indexOf(origin) > -1){
-        res.setHeader('Access-Control-Allow-Origin', origin);
-    }
-
-    // res.setHeader('Access-Control-Allow-Origin', 'http://131.130.37.20:9594');
+    res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
     res.setHeader('Access-Control-Allow-Credentials', true);
