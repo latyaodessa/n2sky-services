33bdb64e60c3931133fa1b335d021a17a6a78feb
Andrii Fedorenko <a1349252@unet.univie.ac.at>
2017-10-12 17:38:45 +0200

    local server

--- frontend/src/restclient/abstract-rest-client.jsx
+++ frontend/src/restclient/abstract-rest-client.jsx
@@ -1,7 +1,7 @@
 //LOCAL
 // export const HOST = 'http://localhost:9595/api/';
 //PROD
-export const HOST = 'http://131.130.37.20:9595/api/';
+export const HOST = 'http://localhost:9595/api/';
 
 export const MONITORING_API = HOST + 'monitoring';
 export const USER_API = HOST + 'user';
